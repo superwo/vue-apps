ae788de9da3877e4b25b6e75dd48a66ca34848ef
superwo <avadanii@tiscali.it>
2023-01-20 11:51:54 +0200

    little style change

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar.vue";
 </script>
 
 <template>
-    <div class="bg-gray-100 h-full">
+    <div class="bg-gray-100 min-h-full">
         <Navbar />
         <router-view />
     </div>
